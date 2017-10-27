45cb3c847f41b3faadaa5180bf590b2f8613d69a
DAITY <33141232+DAITY@users.noreply.github.com>
2017-10-27 10:20:28 +0800

    Add files via upload

---
+++ Solutions.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Solutions.java
+ *
+ * Solutions application
+ *
+ * @author 
+ * @version 1.00 2017/10/27
+ */
+ 
+public class Solutions {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
